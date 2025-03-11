446beb5f512db8a21c75f59f325b2f7ae8d2bc66
0xd1rac <ttz2000@gmail.com>
2025-03-11 23:51:08 +0800

    added parser

--- assembler/parser.py
+++ assembler/parser.py
@@ -4,3 +4,10 @@ class Parser:
     """Parses assembly lines into instructions or label definitions."""
     label_pattern = re.compile(r'^(\w+):')
     instruction_pattern = re.compile(r'^\s*(\w+)(.*)$')
+
+    @classmethod
+    def parse_line(cls, line, line_num):
+        # Remove comments (anything after ';') and strip whitespace.
+        line = line.split(';')[0].strip()
+        if not line:
+            return None
